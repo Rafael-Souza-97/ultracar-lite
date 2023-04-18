92a56f4ace9ff7f3e8e298ed8d3e66ad9f45ef1e
Rafael Souza <souza_rafael.97@outlook.com>
2023-04-18 16:58:08 -0300

    add: allEmployees page

--- frontend/src/Mocks/Employees/Employees.mock.jsx
+++ frontend/src/Mocks/Employees/Employees.mock.jsx
@@ -4,7 +4,7 @@ const mockEmployees = [
     cpf: '123.456.789-00',
     cel: '(11) 99999-9999',
     office: 'Mecânico',
-    wage: 'R$ 3.000,00',
+    wage: '3.000,00',
     startDate: '01/01/2020',
     benefits: 'Vale transporte',
     address: 'Rua A, nº 123'
@@ -14,7 +14,7 @@ const mockEmployees = [
     cpf: '987.654.321-00',
     cel: '(21) 88888-8888',
     office: 'Atendente',
-    wage: 'R$ 1.500,00',
+    wage: '1.500,00',
     startDate: '01/05/2021',
     benefits: 'Vale alimentação',
     address: 'Av. B, nº 456'
@@ -24,7 +24,7 @@ const mockEmployees = [
     cpf: '111.222.333-44',
     cel: '(31) 77777-7777',
     office: 'Mecânico',
-    wage: 'R$ 3.500,00',
+    wage: '3.500,00',
     startDate: '01/01/2019',
     benefits: 'Vale transporte, vale refeição',
     address: 'Rua C, nº 789'
@@ -34,7 +34,7 @@ const mockEmployees = [
     cpf: '444.555.666-77',
     cel: '(41) 66666-6666',
     office: 'Atendente',
-    wage: 'R$ 1.800,00',
+    wage: '1.800,00',
     startDate: '01/02/2020',
     benefits: 'Vale transporte',
     address: 'Av. D, nº 1010'
@@ -44,7 +44,7 @@ const mockEmployees = [
     cpf: '777.888.999-00',
     cel: '(51) 55555-5555',
     office: 'Mecânico',
-    wage: 'R$ 4.000,00',
+    wage: '4.000,00',
     startDate: '01/01/2018',
     benefits: 'Vale transporte, vale refeição, plano de saúde',
     address: 'Rua E, nº 111'
@@ -54,7 +54,7 @@ const mockEmployees = [
     cpf: '222.333.444-55',
     cel: '(11) 99999-8888',
     office: 'Estagiária',
-    wage: 'R$ 900,00',
+    wage: '900,00',
     startDate: '01/06/2021',
     benefits: '',
     address: 'Av. F, nº 1212'
@@ -64,7 +64,7 @@ const mockEmployees = [
     cpf: '555.666.777-88',
     cel: '(21) 88888-7777',
     office: 'Mecânico',
-    wage: 'R$ 3.200,00',
+    wage: '3.200,00',
     startDate: '01/01/2021',
     benefits: 'Vale transporte',
     address: 'Rua G, nº 1414'
@@ -74,7 +74,7 @@ const mockEmployees = [
     cpf: '888.999.000-11',
     cel: '(31) 77777-8888',
     office: 'Atendente',
-    wage: 'R$ 1.500,00',
+    wage: '1.500,00',
     startDate: '01/05/2019',
     benefits: 'Vale transporte, vale refeição',
     address: 'Av. H, nº 1616'
--- frontend/src/pages/NavBar/Employees/AllEmployees.jsx
+++ frontend/src/pages/NavBar/Employees/AllEmployees.jsx
@@ -1,9 +1,41 @@
-import React from 'react';
+import React from 'react'
+import mockEmployees from '../../../Mocks/Employees/Employees.mock'
 
 function AllEmployees() {
   return (
-    <div>AllEmployees</div>
-  )
+    <div className="overflow-x-auto py-10 mt-1 pt-24 mb-52">
+      <h1 className="mb-14 text-gray-600 text-center text-4xl font-light">
+        Colaboradores
+      </h1>
+      <table className="table-auto w-full">
+        <thead>
+          <tr>
+            <th className="px-4 py-2">Nome</th>
+            <th className="px-4 py-2">CPF</th>
+            <th className="px-4 py-2">Telefone</th>
+            <th className="px-4 py-2">Cargo</th>
+            <th className="px-4 py-2">Salário</th>
+            <th className="px-4 py-2">Data de início</th>
+            <th className="px-4 py-2">Benefícios</th>
+            <th className="px-4 py-2">Endereço</th>
+          </tr>
+        </thead>
+        <tbody>
+          {mockEmployees.map((employee, index) => (
+            <tr key={index} className={index % 2 === 0 ? 'bg-gray-100' : 'bg-white'}>
+              <td className="border px-4 py-2">{employee.name}</td>
+              <td className="border text-center first-line:px-4 py-2">{employee.cpf}</td>
+              <td className="border text-center px-4 py-2">{employee.cel}</td>
+              <td className="border text-center px-4 py-2">{employee.office}</td>
+              <td className="border text-center file:px-4 py-2">{employee.wage}</td>
+              <td className="border text-center px-4 py-2">{employee.startDate}</td>
+              <td className="border text-center px-4 py-2">{employee.benefits}</td>
+              <td className="border text-center px-4 py-2">{employee.address}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
-
 export default AllEmployees;
